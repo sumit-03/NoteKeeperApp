8d232e727f1434d7be426e7bcdaba7ae322008fe
sumit-03 <sk69@iitbbs.ac.in>
2021-06-04 14:57:27 +0530

    Updates

---
+++ precache-manifest.71b3685669015b53657a6b12d8211872.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d826f734ad5d8d9a25f1e5e239f5cf22",
+    "url": "/NoteKeeperApp/index.html"
+  },
+  {
+    "revision": "e15ae316479f044dae8f",
+    "url": "/NoteKeeperApp/static/js/2.d5047581.chunk.js"
+  },
+  {
+    "revision": "6be6d28deee41d737c87",
+    "url": "/NoteKeeperApp/static/js/main.03a97737.chunk.js"
+  },
+  {
+    "revision": "199f78fe655182134e99",
+    "url": "/NoteKeeperApp/static/js/runtime-main.3cd7a1d2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NoteKeeperApp/precache-manifest.e06658918fae01e99b2f4450e82ce1e9.js"
+  "/NoteKeeperApp/precache-manifest.71b3685669015b53657a6b12d8211872.js"
 );
 
 self.addEventListener('message', (event) => {
