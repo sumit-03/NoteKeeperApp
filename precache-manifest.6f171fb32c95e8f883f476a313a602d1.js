ea4fe77689314a03ccfea3c13819281f818833c2
sumit-03 <sk69@iitbbs.ac.in>
2021-06-04 10:32:14 +0530

    Updates

---
+++ precache-manifest.6f171fb32c95e8f883f476a313a602d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3681914966c97ae779012f98a61c547e",
+    "url": "/NoteKeeperApp/index.html"
+  },
+  {
+    "revision": "db7f6e12f57db34113bb",
+    "url": "/NoteKeeperApp/static/js/2.94531549.chunk.js"
+  },
+  {
+    "revision": "440c9f7b9b5453a63e9f",
+    "url": "/NoteKeeperApp/static/js/main.f4e11d05.chunk.js"
+  },
+  {
+    "revision": "03fbd7386fe8509e0d4e",
+    "url": "/NoteKeeperApp/static/js/runtime-main.57cbaf78.js"
+  }
+]);
